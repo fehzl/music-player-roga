ed10ba26193bae018b68ef9409af1edc2929194a
Felipe Feliciano <fehzl@live.com>
2022-02-11 08:49:01 +0000

    feat: iniciando a classe Playlist garantindo sucesso em todos os seus testes

--- src/models/Playlist.ts
+++ src/models/Playlist.ts
@@ -1,3 +1,21 @@
+import { Album } from "./Album";
+
 export class Playlist implements PlaylistType {
-  // Code here...
+  readonly albums: AlbumType[];
+
+  constructor(albums: AlbumType[] = []) {
+    this.albums = albums;
+  }
+
+  addAlbum(data: AlbumData) {
+    this.albums.push(new Album(data));
+  }
+
+  isFirstAlbum(index: number): boolean {
+    return index === 0;
+  }
+
+  isLastAlbum(index: number): boolean {
+    return index === this.albums.length - 1;
+  }
 }
