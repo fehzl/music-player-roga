2130d9874fe735d875bf17452162c2cf3d4dfdb6
Felipe Feliciano <fehzl@live.com>
2022-02-11 08:48:42 +0000

    feat: iniciando a classe Albuns garantindo sucesso em todos os seus testes

--- src/models/Album.ts
+++ src/models/Album.ts
@@ -1,3 +1,28 @@
 export class Album implements AlbumType {
-  // Code here...
+  readonly artist: string;
+  readonly cover: string;
+  readonly title: string;
+  readonly tracks: TrackData[];
+
+  constructor(data: AlbumData) {
+    this.artist = data.artist;
+    this.cover = data.cover;
+    this.title = data.title;
+    this.tracks = data.tracks;
+  }
+
+  getUrlFromIndex(index: number): string | null {
+    const track = this.tracks[index];
+    return track ? track.url : null;
+  }
+
+  isFirstTrack(index: number): boolean {
+    const track = this.tracks[index];
+    return track && track.url === this.tracks[0].url;
+  }
+
+  isLastTrack(index: number): boolean {
+    const track = this.tracks[index];
+    return track && track.url === this.tracks[this.tracks.length - 1].url;
+  }
 }
