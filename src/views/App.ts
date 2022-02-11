45913c039d789c0026a4eed21c63b7f0418badcd
Felipe Feliciano <fehzl@live.com>
2022-02-11 12:21:26 +0000

    refactor: remoção do componente Header

--- src/views/App.ts
+++ src/views/App.ts
@@ -1,12 +1,8 @@
 import { html } from '~/utils';
-import { Header } from './Header';
 import './App.css';
 
 export function App() {
   return html`
-    <div class="App">
-      ${Header()}
-      <p>Render you app here...</p>
-    </div>
+    <div class="App"></div>
   `;
 }
