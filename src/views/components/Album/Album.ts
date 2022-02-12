4202e193be28325241b5976e15b37233b80ab5e2
Felipe Feliciano <fehzl@live.com>
2022-02-12 06:55:14 +0000

    feat: passando informações dinâmicas para o componente

--- src/views/components/Album/Album.ts
+++ src/views/components/Album/Album.ts
@@ -1,32 +1,28 @@
 import { html } from "~/utils";
 import "./Album.css";
 
-export function Album() {
+interface AlbumProps {
+  album: AlbumData;
+}
+
+export function Album({ album }: AlbumProps) {
   return html`
     <div class="album-wrapper">
       <div class="album-header">
-        <img src="/img/beethoven.png" width="92px" height="92px" alt="Album">
+        <img src="${album.cover}" width="92px" height="92px" alt="Album">
         <div class="album-header--album-info">
-          <h1 class="album-header--title">Symphony<br/> Collection</h1>
-          <h2 class="album-header--subtitle">Ludwig van Beethoven</h2>
+          <h1 class="album-header--title">${album.title}</h1>
+          <h2 class="album-header--subtitle">${album.artist}</h2>
         </div>
       </div>
       <ul class="album-tracks">
-        <li class="active">
-          <span>
-          01. Symphony no. 1 in C, Op. 21 - I. Adagio molto - Allegro con brio</li>
-          </span>
-        </li>
-        <li>
-          <span>
-            02. Symphony No. 3 in E Flat Major Eroica, Op. 55 - II. Marcia funebre Adagio assai</li>
-          </span>
-        </li>
-        <li>
-          <span>
-            03. Symphony no. 4 in Bb, Op. 60 - IV. Allegro ma non troppo</li>
-          </span>
-        </li>
+        ${album.tracks.map((track, index) => {
+          return html`
+            <li class="album-tracks--track">
+              <span>${index + 1}. ${track.title}</span>
+            </li>
+          `;
+        }).join('')}
       </ul>
     </div>
   `;
