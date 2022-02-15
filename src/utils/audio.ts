0f8d6dbd71295108ed0a799c3d75b0fe9a204717
Felipe Feliciano <fehzl@live.com>
2022-02-15 06:58:29 +0000

    refactor: simplificando o código

--- src/utils/audio.ts
+++ src/utils/audio.ts
@@ -1,4 +1,5 @@
 import { Player as PlayerModel } from '~/models/Player';
+import { addActiveClassToCurrentTrack, changeImageElementSrc, innerHTMLByClass } from './helper';
 
 interface PlayAudioProps {
   player: PlayerModel;
@@ -6,48 +7,24 @@ interface PlayAudioProps {
 }
 
 export async function playAudio({ player, audio }: PlayAudioProps) {
-  const playingNow = document.querySelector('.player-controls--info p');
-  const playButton = <HTMLImageElement>(
-    document.getElementById('player-controls--play')
-  );
-
-  const currentTrack = document.querySelector(
-    `#album-${player.albumIndex}  #track-${player.trackIndex}`
-  );
-
-  // remove active class from all tracks and add it to the current track
-  const tracks = document.querySelectorAll('.album-tracks--track');
-  tracks.forEach((track) => {
-    track.classList.remove('active');
-  });
-  currentTrack?.classList.add('active');
-
-
-  if (currentTrack) {
-    currentTrack.classList.add('active');
-  }
-
-
-  playButton!.src = '/img/pause.svg';
-
-  if (player.trackUrl && player.trackUrl !== audio.src) {
-    audio.src = player.trackUrl;
-  }
+  if (player.trackUrl && player.trackUrl !== audio.src) audio.src = player.trackUrl;
+  
+  changeImageElementSrc('player-controls--play', '/img/pause.svg');
+  
+  addActiveClassToCurrentTrack('.album-tracks--track', `#album-${player.albumIndex} #track-${player.trackIndex}`)
 
   audio.play();
   player.play();
 
-  playingNow!.innerHTML = `${player.album?.artist} - ${
-    player.album?.tracks[player.trackIndex].title
-  }`;
+  innerHTMLByClass('.player-controls--info p', 
+  `${player.album?.tracks[player.trackIndex].title} - 
+   ${player.album?.artist}`
+  );
 }
 
 export function pauseAudio({ player, audio }: PlayAudioProps) {
-  const playButton = <HTMLImageElement>(
-    document.getElementById('player-controls--play')
-  );
-
+  changeImageElementSrc('player-controls--play', '/img/play.svg');
+  
   audio.pause();
   player.pause();
-  playButton!.src = '/img/play.svg';
 }
